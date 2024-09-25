0610f6a38deb62af1ed2fb485a2cdbfff6f88a3b
Osama Heykal <oneheykal@gmail.com>
2024-09-25 22:06:20 +0300

    feat(main-layout): add mainlayout component

--- src/app/components/main-layout/main-layout.component.ts
+++ src/app/components/main-layout/main-layout.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { PostsComponent } from '../posts/posts.component';
+import { HeaderComponent } from '../header/header.component';
+import { RouterOutlet } from '@angular/router';
+import { SidenavComponent } from '../sidenav/sidenav.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [PostsComponent],
+  imports: [PostsComponent, HeaderComponent, RouterOutlet, SidenavComponent],
   templateUrl: './main-layout.component.html',
-  styleUrl: './main-layout.component.scss'
+  styleUrl: './main-layout.component.scss',
 })
-export class MainLayoutComponent {
-
-}
+export class MainLayoutComponent {}
