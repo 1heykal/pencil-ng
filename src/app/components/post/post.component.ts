256d11b875970c8addcf06e5fe8e4a2da0672f55
Osama Heykal <oneheykal@gmail.com>
2024-09-25 22:02:49 +0300

    feat(post): add post component.

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -4,11 +4,22 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { Post } from '../../../models/post';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatChipsModule, MatProgressBarModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatProgressBarModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule
+  ],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
