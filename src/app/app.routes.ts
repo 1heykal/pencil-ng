a76bfe718f4c1ac07ef6b7e30a07ead698c947c0
Osama Heykal <oneheykal@gmail.com>
2024-09-25 22:11:12 +0300

    feat(routes): add routes for PostsComponent and PostComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PostsComponent,
+  },
+
+  {
+    path: 'home',
+    component: PostsComponent,
+  },
+
+  {
+    path: 'post/:pid',
+    component: PostComponent,
+  },
+];
